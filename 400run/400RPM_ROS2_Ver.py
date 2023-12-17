8a027b5b7a8587dddb44260504bb4a987186a02b
Mwesi <mwesi@yahoo.com>
2023-12-17 17:30:17 -0500

    Update 400RPM_ROS2_Ver.py

--- 400run/400RPM_ROS2_Ver.py
+++ 400run/400RPM_ROS2_Ver.py
@@ -1,33 +1,39 @@
-import rospy
+import rclpy
+from rclpy.node import Node
 from std_msgs.msg import Float64
 import time
 
-def run_vesc():
-    # Initialize ROS node
-    rospy.init_node('vesc_speed_controller', anonymous=True)
+class VescSpeedController(Node):
+    def __init__(self):
+        super().__init__('vesc_speed_controller')
+        self.speed_pub = self.create_publisher(Float64, '/vesc/speed', 10)
+        self.timer = self.create_timer(0.1, self.timer_callback) # 10 Hz
+        self.end_time = self.get_clock().now() + rclpy.duration.Duration(seconds=10)
+        self.speed = Float64()
+        self.speed.data = 400
+        self.running = True
 
-    # Create a publisher to send speed commands to VESC
-    speed_pub = rospy.Publisher('/vesc/speed', Float64, queue_size=10)
+    def timer_callback(self):
+        if self.get_clock().now() < self.end_time and self.running:
+            self.speed_pub.publish(self.speed)
+        else:
+            if self.running:
+                # Stop the motor
+                self.speed.data = 0
+                self.speed_pub.publish(self.speed)
+                self.running = False
 
-    # Set the rate of publishing messages
-    rate = rospy.Rate(10) # 10 Hz
+def main(args=None):
+    rclpy.init(args=args)
+    vesc_speed_controller = VescSpeedController()
 
-    # Set speed in RPM
-    speed = Float64()
-    speed.data = 400
-
-    # Run the motor at 400 RPM for 10 seconds
-    end_time = rospy.Time.now() + rospy.Duration(10)
-    while rospy.Time.now() < end_time:
-        speed_pub.publish(speed)
-        rate.sleep()
-
-    # Stop the motor
-    speed.data = 0
-    speed_pub.publish(speed)
+    try:
+        rclpy.spin(vesc_speed_controller)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        vesc_speed_controller.destroy_node()
+        rclpy.shutdown()
 
 if __name__ == '__main__':
-    try:
-        run_vesc()
-    except rospy.ROSInterruptException:
-        pass
+    main()
