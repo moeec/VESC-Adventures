8cedf75a1ffc59648d6879bcf219242775d992c7
Mwesi <mwesi@yahoo.com>
2024-06-13 10:26:55 -0400

    Add files via upload

--- scripts/start_vesc_driver.sh
+++ scripts/start_vesc_driver.sh
@@ -1,15 +1,31 @@
 #!/bin/sh
 
+# Load Some Parameters for Ackermann
+
+rosparam set /speed_to_erpm_gain 0.5
+rosparam set /speed_to_erpm_offset 0.2
+rosparam set /steering_angle_to_servo_gain  0.2
+rosparam set /steering_angle_to_servo_offset 0.1
+rosparam set /wheelbase 0.8
+
+
 # launch vesc_driver_node.launch
 
 xterm -e "cd $(pwd)/../..;
 source devel/setup.bash;
 roslaunch vesc_driver vesc_driver_node.launch " & 
 
-sleep 15
+sleep 5
 
-# launch gmapping_demo.launch to perform slam_gmapping
+# launch vesc_driver_nodelet 
 
-xterm -hold -e  "cd $(pwd)/../..;
+xterm -e  "cd $(pwd)/../..;
 source devel/setup.bash;
-roslaunch vesc_driver vesc_driver_nodelet.launch "
+roslaunch vesc_driver vesc_driver_nodelet.launch " &
+
+
+# launch vesc_driver_nodelet 
+
+xterm -e  "cd $(pwd)/../..;
+source devel/setup.bash;
+roslaunch vesc_ackermann vesc_to_odom_node.launch "
